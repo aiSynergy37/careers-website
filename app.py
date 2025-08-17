bfaead3ede38ae31508c89d42a44a14d9b9e25a5
aiSynergy37 <m.econdse@gmail.com>
2025-08-17 14:53:49 +0530

    Initial Flask app with jobs API

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, jsonify
+app = Flask(__name__)
+
+# we need to create an app
+# an app is simply an object of class Flask
+
+# import Flask n create an object of it
+# wt u need to flask
+# wen a certain url is requested wt shld be rewtuenec
+# route is path after domain name
+
+JOBS = [
+	{
+	'id':1,
+	'title':'Data Analyst',
+	'location':'Bengaluru, India',
+	'salary': 'Rs. 10, 00, 000'
+	},
+	{
+	'id':2,
+	'title':'Data Scientist',
+	'location':'Delhi, India',
+	'salary': 'Rs. 15, 00, 000'
+	},
+	{
+	'id':3,
+	'title':'Frontend Engineer',
+	'location':'Remote',
+	'salary': 'Rs. 12, 00, 000'
+	},
+	{
+	'id':4,
+	'title':'Backend Engineer',
+	'location':'San Francisco, USA',
+	'salary': 'USD 120, 000'
+	}
+]
+
+@app.route("/")
+def hello_world():
+	return render_template("home.html", jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+	return jsonify(JOBS)
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', debug=True)
